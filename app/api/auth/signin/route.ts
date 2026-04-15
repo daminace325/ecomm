c7158745af6116cf409fa1f85a7ad7b701d1e06f
Damin <mohammadd@maqsoftware.com>
2026-04-15 09:54:33 +0530

    Implemented POST API route for user signin

---
+++ app/api/auth/signin/route.ts
@@ -0,0 +1,61 @@
+import { usersCollection } from "@/lib/collections";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+const cookieName = "token";
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const { email, password } = body;
+
+        if (!email || typeof email !== "string") {
+            return NextResponse.json({ error: "Email is required" }, { status: 400 });
+        }
+
+        if (!password || typeof password !== "string") {
+            return NextResponse.json({ error: "Password is required" }, { status: 400 });
+        }
+
+        const users = await usersCollection();
+
+        const user = await users.findOne({ email });
+        if (!user) {
+            return NextResponse.json({ error: "Invalid Credentials" }, { status: 401 });
+        }
+
+        const isValid = await bcrypt.compare(password, user.passwordHash);
+        if (!isValid) {
+            return NextResponse.json({ error: "Invalid Credentials" }, { status: 401 });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        const res = NextResponse.json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+        res.cookies.set(cookieName, token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            path: "/"
+        });
+
+        return res;
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to signin" }, { status: 500 });
+    }
+}
